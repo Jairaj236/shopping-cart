fe5ef433d2c31e96ce8877c832996814285855ee
Jaya Raju <svara.jayaraju.actualpixel.in>
2025-09-05 20:41:18 +0530

    cahnges applied

--- shoppingMall/fetures/main.js
+++ shoppingMall/fetures/main.js
@@ -15,11 +15,11 @@
           <div class="price-quantity">
             <h2>$ ${price} </h2>
             <div class="buttons">
-              <button onclick="decrement('${id}')" class="bi bi-plus-lg">--</button>
+              <button onclick="decrement(${id})" class="bi bi-dash-lg">--</button>
               <div id=${id} class="quantity">
              0
               </div>
-              <button onclick="increment('${id}')" class="bi bi-plus-lg">+</button>
+              <button onclick="increment(${id})" class="bi bi-plus-lg">+</button>
             </div>
           </div>
         </div>
@@ -29,8 +29,9 @@
      }).join("")
 
      const decrement = (id)=>{
-         
-             const remCart =   cart.find((data)=>data.id ===id)
+               const selectedId = id.id
+
+             const remCart =   cart.find((data)=>data.id === selectedId)
     console.log(remCart)
 
              if(remCart !== undefined){
@@ -38,17 +39,17 @@
              }
             
              updateCart(id)
-   console.log(cart)
-
+       console.log(cart)
      }
 
    const  increment = (id)=>{
-      console.log(id)
-     const search =  cart.find((dta,index) => dta.id === id )
+      // console.log(id)
+      const selectedId = id.id
+     const search =  cart.find((dta,index) => dta.id === selectedId )
          
      if(search === undefined){
         cart.push({
-            id: id,
+            id: selectedId,
             item : 1,
         })
      }
@@ -59,10 +60,56 @@
       updateCart(id)
    }
 
-   const updateCart = (id)=>{
-   
-           const updateCartValues   =  cart.find((data)=> data.id === id)
-          console.log(updateCartValues.item)
-           document.querySelector(".quantity").textContent = updateCartValues.item
-       
-   }
+  //  const updateCart = (id)=>{
+  //   console.log("id",id)
+  //          let updateCartValues =  cart.find((data,index)=> data.id === id.id)
+  //         console.log("@@",updateCartValues)
+  //         id.innerHTML = updateCartValues.item
+
+  //         const incrementbtn =   document.getElementById(id.id)
+  //         // console.log(value)
+  //         if(updateCartValues.item >= 10 ){
+  //            incrementbtn.disabled = true;
+  //         }
+  //  }
+
+
+  const updateCart = (id) => {
+    // id is the div element, so id.id gives its string ID.
+    const quantityDiv = document.getElementById(id.id);
+
+    // Find the item in the cart.
+    const updateCartValues = cart.find((data) => data.id === id.id);
+
+    if (updateCartValues && quantityDiv) {
+        // Update the quantity display.
+        quantityDiv.innerHTML = updateCartValues.item;
+
+
+        const parent = quantityDiv.parentElement;
+        const incrementbtn = parent.querySelector('.bi-plus-lg');
+       const decrementbtn = parent.querySelector('.bi-dash-lg');
+
+
+        if (updateCartValues.item === 10) {
+            if (incrementbtn) {
+                incrementbtn.disabled = true;
+            }
+        } else {
+            if (incrementbtn) {
+                incrementbtn.disabled = false;
+            }
+        }
+
+
+         if (updateCartValues.item  <= 0) {
+            if (decrementbtn) {
+                decrementbtn.disabled = true;
+            }
+        } else {
+            if (incrementbtn) {
+                decrementbtn.disabled = false;
+            }
+        }
+    }
+};
